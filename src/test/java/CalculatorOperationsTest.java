e4d5f99c98bdbc80e3ce1973891775e2fb662c92
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-08-21 14:53:08 +0300

    calculator operations2

--- src/test/java/CalculatorOperationsTest.java
+++ src/test/java/CalculatorOperationsTest.java
@@ -1,20 +1,19 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class CalculatorTests {
+public class CalculatorOperationsTest {
     AppiumDriver<MobileElement> driver;
-    @Test
-    public void test() throws MalformedURLException, InterruptedException {
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
         DesiredCapabilities caps =new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 3");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
@@ -23,25 +22,20 @@ public class CalculatorTests {
 
         URL url = new URL("http://localhost:4723/wd/hub");
         driver = new AndroidDriver<MobileElement>(url, caps);
-        //System.out.println(driver.getTitle());
-        System.out.println(driver.getDeviceTime());
-        Assertions.assertEquals("android",driver.getPlatformName());
 
-        Thread.sleep(4000);
-         MobileElement clearElem=driver.findElement(MobileBy.AccessibilityId("clear"));
+    }
 
-        System.out.println(clearElem.getText());
-
-        Assertions.assertTrue(clearElem.isDisplayed());
-
-        Assertions.assertEquals("AC",clearElem.getText());
-
-
-
-        driver.closeApp();
-
-
+    @Test
+    public void addTest() {
 
 
     }
+
+    @AfterEach
+    public void cleanUp() {
+        driver.closeApp();
+
+
+    }
+
 }
