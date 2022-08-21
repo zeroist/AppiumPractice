48bf8f5b28a39b5a7abd8a9c7a7f5682990b53d1
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-08-21 11:28:34 +0300

    calculator succescully opened.that means our framework works fine

---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,27 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CalculatorTests {
+    AppiumDriver<MobileElement> driver;
+    @Test
+    public void test() throws MalformedURLException {
+        DesiredCapabilities caps =new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 3");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,",10.0");
+        caps.setCapability(MobileCapabilityType.APP," https://cybertek-appium.s3.amazonaws.com/calculator.apk");
+
+        URL url = new URL("http://localhost:4723/wd/hub");
+        driver = new AndroidDriver<MobileElement>(url, caps);
+
+
+
+    }
+}
